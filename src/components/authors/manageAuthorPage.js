98f81dff7f4ff04cc9b8739eb3742d95cee8c97b
Marmiz <claudio.restifo@gmail.com>
2017-01-19 21:40:46 +0700

    validation and leave page warnings

--- src/components/authors/manageAuthorPage.js
+++ src/components/authors/manageAuthorPage.js
@@ -5,29 +5,71 @@ var ReactRouter = require('react-router');
 var browserHistory = ReactRouter.browserHistory; //for programmatic redirects
 var AuthorForm = require('./AuthorForm');
 var AuthorApi = require('../../api/authorApi');
+var toastr = require('toastr');
 
 var ManageAuthorPage = React.createClass({
   // since this is my top-level component
   // here's where we handle change into form
+  componentDidMount: function() {
+		this.props.router.setRouteLeaveHook(this.props.route, this.routerWillLeave);
+	},
+
+	routerWillLeave: function(nextLocation) {
+		// Return false to prevent a transition w/o prompting the user,
+		// or return a string to allow the user to decide:
+		if (this.state.dirty) {
+			return 'Leave without saving?';
+		}
+		return true;
+	},
   getInitialState: function () {
     return {
       author: {id: '', firstName: '', lastName: ''},
       errors: {},
+      dirty: false
     }
   },
   // check and update
   setAuthorState: function(event) {
+    this.setState({dirty: true});
     var field = event.target.name;
     var value = event.target.value;
     this.state.author[field] = value;
     return this.setState({author: this.state.author});
   },
 
+  // basic validation
+  authorFormIsValid: function() {
+    var formIsValid = true;
+    this.state.errors = {}; // clear any previous error
+
+    if (this.state.author.firstName.length < 2) {
+			this.state.errors.firstName = 'First name must be at least 2 characters.';
+			formIsValid = false;
+		}
+
+		if (this.state.author.lastName.length < 2) {
+			this.state.errors.lastName = 'Last name must be at least 2 characters.';
+			formIsValid = false;
+		}
+
+    this.setState({errors: this.state.errors});
+		return formIsValid;
+  },
+
   // Save
   saveAuthor: function(event) {
     event.preventDefault();
-    AuthorApi.saveAuthor(this.state.author);
-    browserHistory.push('/authors');
+
+    if (!this.authorFormIsValid()) {
+      return;
+    }
+
+    this.setState({dirty: false}, function() {
+			toastr.success('Author saved.');
+			browserHistory.push('/authors');
+		});
+
   },
 
   render: function() {
