9bfe21ecf9b4a231081f1f3bb0d2af7d9ccd7915
Marmiz <claudio.restifo@gmail.com>
2017-02-03 21:18:51 +0700

    actions and costants initial commit

---
+++ src/actions/authorActions.js
@@ -0,0 +1,22 @@
+"use strict";
+
+var Dispatcher = require('../dispatcher/appDispatcher');
+var AuthorApi = require('../api/authorApi');
+var ActionTypes = require('../constants/actionTypes');
+
+var AuthorActions = {
+
+  createAuthor: function(author) {
+		var newAuthor = AuthorApi.saveAuthor(author);
+
+		//Hey dispatcher, go tell all the stores that an author was just created.
+    //reference the action from ../constants/actionTypes
+		Dispatcher.dispatch({
+			actionType: ActionTypes.CREATE_AUTHOR,
+			author: newAuthor
+		});
+	},
+}
+
+
+module.exports = AuthorActions;
---
+++ src/constants/actionTypes.js
@@ -0,0 +1,8 @@
+"use strict";
+
+// use key mirror to avoid typing key and value
+ var keyMirror = require('react/lib/keyMirror');
+
+module.exports = keyMirror({
+ CREATE_AUTHOR: null,
+});
